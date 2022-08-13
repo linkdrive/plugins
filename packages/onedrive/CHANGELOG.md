937154c0ecb37dc43af289583c67be15761d4df1
wuting <reruin@gmail.com>
2022-08-13 21:35:26 +0800

    release: sharelist-plugin-onedrive@0.2.4

---
+++ packages/onedrive/CHANGELOG.md
@@ -0,0 +1,9 @@
+## 0.2.4 (2022-08-13)
+
+
+### Features
+
+* init repo ([c7209dd](https://github.com/linkdrive/sharelist-plugin/commit/c7209dd0b2b7f1e15fa712558b1ac676e598982c))
+
+
+
---
+++ scripts/release.js
@@ -0,0 +1,162 @@
+
+
+ const execa = require('execa')
+ const path = require('path')
+ const fs = require('fs')
+ const args = require('minimist')(process.argv.slice(2))
+ const semver = require('semver')
+ const chalk = require('chalk')
+ const prompts = require('prompts')
+ 
+ const pkgDir = process.cwd()
+ const pkgPath = path.resolve(pkgDir, 'package.json')
+ 
+ const remote = 'origin'
+ 
+ /**
+  * @type {{ name: string, version: string }}
+  */
+ const pkg = require(pkgPath)
+ const pkgName = pkg.name.replace(/^@sharelist\//, '')
+ const currentVersion = pkg.version
+ const isDryRun = args.dry
+ 
+ const skipBuild = args.skipBuild
+  
+ const commitPath = args['commit-path']
+ 
+ /**
+  * @type {import('semver').ReleaseType[]}
+  */
+ const versionIncrements = [
+   'patch',
+   'minor',
+   'major',
+   'prepatch',
+   'preminor',
+   'premajor',
+   'prerelease'
+ ]
+ 
+ 
+ const inc = (i) => semver.inc(currentVersion, i, 'beta')
+ 
+ const run = isDryRun ? (bin, args, opts = {}) =>
+   console.log(chalk.blue(`[dryrun] ${bin} ${args.join(' ')}`), opts) : (bin, args, opts = {}) =>
+   execa(bin, args, { stdio: 'inherit', ...opts })
+ 
+ 
+ const step = (msg) => console.log(chalk.cyan(msg))
+ 
+ async function main() {
+   let targetVersion = args._[0]
+ 
+   if (!targetVersion) {
+     // no explicit version, offer suggestions
+     /**
+      * @type {{ release: string }}
+      */
+     const { release } = await prompts({
+       type: 'select',
+       name: 'release',
+       message: 'Select release type',
+       choices: versionIncrements
+         .map((i) => `${i} (${inc(i)})`)
+         .concat(['custom'])
+         .map((i) => ({ value: i, title: i }))
+     })
+ 
+     if (release === 'custom') {
+       /**
+        * @type {{ version: string }}
+        */
+       const res = await prompts({
+         type: 'text',
+         name: 'version',
+         message: 'Input custom version',
+         initial: currentVersion
+       })
+       targetVersion = res.version
+     } else {
+       targetVersion = release.match(/\((.*)\)/)[1]
+     }
+   }
+ 
+   if (!semver.valid(targetVersion)) {
+     throw new Error(`invalid target version: ${targetVersion}`)
+   }
+ 
+   const tag =
+     pkgName === 'sharelist' ? `v${targetVersion}` : `${pkgName}@${targetVersion}`
+ 
+   if (targetVersion.includes('beta') && !args.tag) {
+     /**
+      * @type {{ tagBeta: boolean }}
+      */
+     const { tagBeta } = await prompts({
+       type: 'confirm',
+       name: 'tagBeta',
+       message: `Publish under dist-tag "beta"?`
+     })
+ 
+     if (tagBeta) args.tag = 'beta'
+   }
+ 
+   /**
+    * @type {{ yes: boolean }}
+    */
+   const { yes } = await prompts({
+     type: 'confirm',
+     name: 'yes',
+     message: `Releasing ${tag}. Confirm?`
+   })
+ 
+   if (!yes) {
+     return
+   }
+ 
+   step('\nUpdating package version...')
+   updateVersion(targetVersion)
+ 
+   step('\nBuilding package...')
+   if (!skipBuild && !isDryRun) {
+     await run('yarn', ['build'])
+   } else {
+     console.log(`(skipped build)`)
+   }
+ 
+   step('\nGenerating changelog...')
+   await run('yarn', ['changelog'])
+ 
+   const { stdout } = await run('git', ['diff'], { stdio: 'pipe' })
+   if (stdout) {
+     step('\nCommitting changes...')
+     await run('git', ['add', '-A'].concat(commitPath || []))
+     await run('git', ['commit', '-m', `release: ${tag}`])
+   } else {
+     console.log('No changes to commit.')
+   }
+ 
+   step('\nPushing to GitHub...')
+   await run('git', ['tag', tag])
+   await run('git', ['push', remote, `refs/tags/${tag}`])
+   await run('git', ['push', remote, 'master'])
+ 
+   if (isDryRun) {
+     console.log(`\nDry run finished - run git diff to see package changes.`)
+   }
+ 
+ }
+ 
+ /**
+  * @param {string} version
+  */
+ function updateVersion(version) {
+   const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
+   pkg.version = version
+   fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n')
+ }
+ 
+ main().catch((err) => {
+   console.error(err)
+ })

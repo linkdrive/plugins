fbc92dfecbc42b6d81d58069206dda4025ca1ed0
Ting <reruin@gmail.com>
2022-08-29 11:39:19 +0800

    update
    chore:

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,18 +1,11 @@
 // https://prettier.io/docs/en/configuration.html
 module.exports = {
   semi: false,
-
   trailingComma: "all",
-
   singleQuote: true,
-
   printWidth: 120,
-
   endOfLine: "auto",
-
   tabWidth: 2,
-
   endOfLine: "auto",
-
   arrowParens: "avoid"
 }
